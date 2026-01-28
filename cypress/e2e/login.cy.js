971794b830b0827f8abcf1bfdd6089d90fed6ab0
unknown <victornesb@outlook.com>
2026-01-28 15:20:56 -0300

    Command abstractio with userData

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,18 +9,32 @@ describe('Orage HRM Tests', () => {
     dashboardGrid : ".orangehrm-dashboard-grid"
   }
 
+  const userData = {
+    userSucess: {
+      username : "Admin",
+      passowrd : "admin123"
+    },
+
+    userFail: {
+      username : "test",
+      password : "test"
+
+    }
+     
+  }
+
   it('login - Sucessfull', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField) .type('Admin')
-    cy.get(selectorsList.passwordField) .type('admin123')
+    cy.get(selectorsList.usernameField) .type(userData.userSucess.username)
+    cy.get(selectorsList.passwordField) .type(userData.userSucess.passowrd)
     cy.get(selectorsList.loginButton) .click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
   })
   it('login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField) .type('test')
-    cy.get(selectorsList.passwordField) .type('test')
+    cy.get(selectorsList.usernameField) .type(userData.userFail.username)
+    cy.get(selectorsList.passwordField) .type(userData.userFail.password)
     cy.get(selectorsList.loginButton) .click()
     cy.get(selectorsList.wrongCredentialAlert)
   })
